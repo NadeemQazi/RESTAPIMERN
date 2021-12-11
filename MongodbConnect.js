c7fae50395b634acca8e3a30cd1429277791b9ee
Nadeem Qazi <nadeemhqazi@gmail.com>
2021-12-11 21:07:47 +0000

    backend

--- MongodbConnect.js
+++ MongodbConnect.js
@@ -1,7 +1,8 @@
 mongoose= require('mongoose')
 
-const MONG_URI= 'mongodb://localhost:27017/Alib'
+//const MONG_URI= 'mongodb://localhost:27017/Alib'
 //const MONG_URI= 'mongodb+srv://nhq:hhqazi2906@cd5006.438bk.mongodb.net/labweek7?retryWrites=true&w=majority'
+const MONG_URI= 'mongodb+srv://nhq:hhqazi2906@cd5006.438bk.mongodb.net/ReplLab?retryWrites=true&w=majority'
 mongoose.connect(MONG_URI,{useUnifiedTopology:true,useNewUrlParser:true,  useFindAndModify: false })
 const db= mongoose.connection;
 db.on('error',function(err){
